f43dbd1358ec9670d1be81993fa1dd408e3b1fdf
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-11 12:59:34 +0800

    Update exercise2.c

--- exercise2.c
+++ exercise2.c
@@ -10,6 +10,17 @@
 //main function section 
 int main(){
     //write here your program
+    int LA[] = {1, 4, 5, 7, 10, 12, 13, 17, 18, 20};
+    int n = 10;
+    int i;
+
+    for(i = 0; i < n; i++){
+        if(LA[i]%2 == 0){
+            printf("LA[%d] = %d \n", i, LA[i]);
+        }
+        
+        
+    }
 
 
     return 0;
