e291fd83ea57abb0e411c63c18b6b678d95d2911
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-11 12:58:33 +0800

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -10,6 +10,14 @@
 //main function section 
 int main(){
     //write here your program
+    int LA[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int n = 10;
+    int i;
+
+    for(i = 0; i < n; i++){
+        printf("LA[%d] = %d \n", i, LA[i]);
+        
+    }
 
 
     return 0;
